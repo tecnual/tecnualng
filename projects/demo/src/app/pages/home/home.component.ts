7d347284f0f41fceffc587d3bcd049ef8b538c4b
Pedro Macías <pedro@tecnual.com>
2025-11-30 01:19:55 +0100

    feat: Migrate home component styles to use CSS variables, import TngButton, adjust theme definitions, and refine button hover effects and sidebar positioning.

--- projects/demo/src/app/pages/home/home.component.ts
+++ projects/demo/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TngButton } from 'tecnualng';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, TngButton],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
